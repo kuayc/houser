e15f22f45c240821969d4918b78f11c14dade9df
Chin Kuay <kuaychin@gmail.com>
2020-04-10 12:45:49 -0500

    added wizard.js

---
+++ src/Components/Wizard/Wizard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../Wizard/Wizard.css";
+
+export default class Wizard extends Component {
+  clearReduxState() {}
+  render() {
+    return (
+      <section className="wizard-new-list">
+        <h1>Add New Listing</h1>
+        <button onClick={this.clearReduxState}>Cancel</button>
+      </section>
+    );
+  }
+}
