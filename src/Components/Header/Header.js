acd318dd9eadd2f89b2ae3848dd19b53211336d0
Chin Kuay <kuaychin@gmail.com>
2020-04-10 12:36:18 -0500

    added header.js

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Header.css";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="header-main">
+        <img src="" alt="logo" />
+        <h1 className="logo">Houser</h1>
+      </div>
+    );
+  }
+}
